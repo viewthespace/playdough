9ab406c03f11de92976135cd7d79608a28e16d80
Nassredean Nasseri <dnasseri10@gmail.com>
2016-02-18 13:02:19 -0500

    no need to load files directly

--- lib/versioner_rails.rb
+++ lib/versioner_rails.rb
@@ -5,18 +5,14 @@ module VersionerRails
   def versioner mod
     self.prepend_before_filter do
       if request.accept
-        version = request.headers['HTTP_X_VERSIONER'].present? ? request.headers['HTTP_X_VERSIONER'].to_i : 1
-        shape = request.headers['HTTP_X_SHAPE']
+        version = request.accept[/version\s?=\s?(\d+)/, 1]
+        shape = request.accept[/shape\s?=\s?(\w+)/, 1]
         mod_version = mod.const_get("V#{version}")
         if shape
-          load "app/serializers/#{mod.to_s.underscore}/v#{version}/#{mod.to_s.underscore}_#{shape}_serializer.rb"
-          klass = mod_version.constants.map{ |c| c.to_s.underscore }.select{|s| s[/_(\w+)_serializer$/, 1] == shape }.first
+          @versioner_serializer = mod_version.const_get("#{mod.name}#{shape.capitalize}Serializer")
         else
-          load "app/serializers/#{mod.to_s.underscore}/v#{version}/#{mod.to_s.underscore}_serializer.rb"
-          type = mod.to_s.demodulize.underscore
-          klass = mod_version.constants.map{ |c| c.to_s.underscore }.select{|s| "#{type}_serializer" == s }.first
+          @versioner_serialzier = mod_version.const_get("#{mod.name}Serializer")
         end
-        @versioner_serializer = mod_version.const_get(klass.classify)
       end
     end
   end
