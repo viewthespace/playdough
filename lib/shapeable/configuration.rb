a33438100bc99fd872c50fbc33fd22370136d907
Nassredean Nasseri <dnasseri10@gmail.com>
2016-04-16 10:17:11 -0400

    add enforce_shape to config

--- lib/shapeable/configuration.rb
+++ lib/shapeable/configuration.rb
@@ -1,13 +1,14 @@
 module Shapeable
   class Configuration
 
-    attr_accessor :path, :default_version, :default_shape, :enforce_versioning
+    attr_accessor :path, :default_version, :default_shape, :enforce_versioning, :enforce_shape
 
     def initialize
       @path = nil
       @default_version = nil
       @default_shape = nil
       @enforce_versioning = true
+      @enfore_shape = true
     end
 
     def as_json
@@ -15,7 +16,8 @@ module Shapeable
         path: path,
         default_version: default_version,
         default_shape: default_shape,
-        enforce_versioning: enforce_versioning
+        enforce_versioning: enforce_versioning,
+        enforce_shape: enforce_shape
       }
     end
   end
